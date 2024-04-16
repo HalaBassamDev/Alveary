6c4616a01f4608794b40816f4f7037a424149503
HalaBassamDev <167170945+HalaBassamDev@users.noreply.github.com>
2024-04-16 17:45:51 +0300

    Add files via upload

---
+++ vars.dart
@@ -0,0 +1,35 @@
+class variable {
+  static List<Map<int, List>> employeesAttend = [
+    {
+      1: [
+        'Micheal Devies',
+        'Grace Wilson',
+        'Olivia Green',
+        'Noor Green',
+        'Emmily warson',
+        'Ali Gosh',
+        'Albert Dera',
+        'Emma warson',
+        'Kareem Saed'
+      ]
+    },
+    {
+      2: [
+        'Mark Devies',
+        'Hadeel Mark',
+        'Daiv Devies',
+        'nooh Devies',
+        'mohammad jason'
+      ]
+    },
+    {
+      3: [
+        'Jenny Wilson',
+        'Golf Mark',
+        'Daiv Devies',
+        'Noor Devies',
+        'Emma Walson'
+      ]
+    }
+  ];
+}
